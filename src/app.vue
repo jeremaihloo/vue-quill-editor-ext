bb93e1ae7e4a0f6c94e71879042b249a91a4987d
jeremaihloo <jeremaihloo1024@gmail.com>
2018-12-12 17:04:32 +0800

    Init commit

---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+import Editor from './components/editor.vue'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+export default {
+  name: 'app',
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/options.js
@@ -0,0 +1,69 @@
+export default {
+  theme: 'snow',
+  boundary: document.body,
+  modules: {
+    imageResize: {
+      modules: ['Resize', 'DisplaySize', 'Toolbar']
+    },
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ header: 1 }, { header: 2 }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ script: 'sub' }, { script: 'super' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ direction: 'rtl' }],
+      [
+        {
+          size: [
+            false,
+            '10px',
+            '12px',
+            '14px',
+            '16px',
+            '18px',
+            '20px',
+            '22px',
+            '24px',
+            '26px',
+            '28px',
+            '30px',
+            '32px',
+            '34px',
+            '36px'
+          ]
+        }
+      ],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }, { background: [] }],
+      [{ font: [] }],
+      [{ align: [] }],
+      ['clean'],
+      ['link', 'image', 'video'],
+      [
+        {
+          lineheight: ['1.0', '1.2', '1.5', '1.6', '1.8', '2.0', '2.4', '2.8', '3.0', '4.0', '5.0']
+        }
+      ],
+      [
+        {
+          letterspacing: [
+            '0px',
+            '1px',
+            '2px',
+            '3px',
+            '4px',
+            '5px',
+            '6px',
+            '7px',
+            '8px',
+            '9px',
+            '10px'
+          ]
+        }
+      ]
+    ]
+  },
+  placeholder: 'Insert text here ...',
+  readOnly: false
+}
